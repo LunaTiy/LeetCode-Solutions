1acb50867b54bd708c0a1d0e3a4de80e5aeb5b04
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2022-12-13 14:16:01 +0300

    Add 931. Minimum Falling Path Sum

---
+++ MinimumFallingPathSum/Program.cs
@@ -0,0 +1,5 @@
+﻿using MinimumFallingPathSum;
+
+int[][] matrix = { new[] {2, 1, 3}, new[] {6, 5, 4}, new[] {7, 8, 9} };
+
+int result = new Solution().MinFallingPathSum(matrix);
---
+++ MinimumFallingPathSum/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace MinimumFallingPathSum;
+
+public class Solution
+{
+    public int MinFallingPathSum(int[][] matrix)
+    {
+        int matrixLength = matrix.Length;
+        var newMatrix = new int[matrixLength][];
+
+        newMatrix[0] = new int[matrixLength];
+        
+        for (var j = 0; j < matrixLength; j++) newMatrix[0][j] = matrix[0][j];
+
+        for (var i = 1; i < matrixLength; i++)
+        {
+            newMatrix[i] = new int[matrixLength];
+            
+            for (var j = 0; j < matrixLength; j++)
+            {
+                int minValue = newMatrix[i - 1][j];
+
+                if (j - 1 >= 0) minValue = Math.Min(minValue, newMatrix[i - 1][j - 1]);
+                if (j + 1 < matrixLength) minValue = Math.Min(minValue, newMatrix[i - 1][j + 1]);
+
+                newMatrix[i][j] = minValue + matrix[i][j];
+            }
+        }
+
+        return newMatrix[^1].Min();
+    }
+}
