01c590cdb9c674243941f7f9aecdcc4fc0b863bd
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-01-20 00:24:08 +0300

    Add "974. Subarray Sums Divisible by K" solution

---
+++ Medium/974. Subarray Sums Divisible by K/SubarraySumsDivisibleByK.Tests/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿namespace SubarraySumsDivisibleByK.Tests;
+
+public class SolutionTests
+{
+    [Test]
+    public void Test1()
+    {
+        int[] nums = { 4, 5, 0, -2, -3, 1 };
+        const int k = 5;
+        const int result = 7;
+        
+        Assert.AreEqual(result, new Solution().SubarraysDivByK(nums, k));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        int[] nums = { 5 };
+        const int k = 9;
+        const int result = 0;
+        
+        Assert.AreEqual(result, new Solution().SubarraysDivByK(nums, k));
+    }
+}
---
+++ Medium/974. Subarray Sums Divisible by K/SubarraySumsDivisibleByK/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace SubarraySumsDivisibleByK;
+
+public class Solution
+{
+    public int SubarraysDivByK(int[] nums, int k)
+    {
+        var remainder = 0;
+        var result = 0;
+        var remainderFrequency = new int[k];
+        remainderFrequency[0] = 1;
+        
+        foreach (int num in nums)
+        {
+            remainder = (remainder + num % k + k) % k;
+            result += remainderFrequency[remainder];
+            remainderFrequency[remainder]++;
+        }
+
+        return result;
+    }
+}
